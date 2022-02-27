5db9f2f41d06e89ef9b72c2d6abad09c6e17c138
fbleech <100531321+fbleech@users.noreply.github.com>
2022-02-28 00:48:15 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -235,8 +235,8 @@ async def call_apropriate_function(
                 os.rename(to_upload_file,
                           f"{CUSTOM_FILE_NAME}{to_upload_file}")
                 to_upload_file = f"{CUSTOM_FILE_NAME}{to_upload_file}"
-                if f_caption is None:
-                    f_caption = f"{CUSTOM_FILE_NAME}{to_upload_file}"
+                if CUSTOM_FILE_NAME is None:
+                    to_upload_file = f"{CUSTOM_FILE_NAME}{to_upload_file}"
                 buttons = [
                     [
                         InlineKeyboardButton('🍿 Channel 🍿', url='t.me/cinesubz')
